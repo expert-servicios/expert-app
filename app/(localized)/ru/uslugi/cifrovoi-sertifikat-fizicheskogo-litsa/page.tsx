a2e9b5132f4ff171685932af15bf525ad05a6482
expert_app <expertestudiospro@gmail.com>
2026-09-19 17:17:47 +0200

    perf: revalidate public service reputation

--- app/(localized)/ru/uslugi/cifrovoi-sertifikat-fizicheskogo-litsa/page.tsx
+++ app/(localized)/ru/uslugi/cifrovoi-sertifikat-fizicheskogo-litsa/page.tsx
@@ -7,6 +7,8 @@ import { shouldIndexLocale } from '@/lib/i18n/feature-flags';
 import { ServiceShareActions } from '@/components/services/ServiceShareActions';
 import { ServiceRatingSummary } from '@/components/services/ServiceRatingSummary';
 
+export const revalidate = 300;
+
 const SERVICE_SLUG = 'certificado-digital-persona-fisica';
 const ES_URL = 'https://expertconsulting.es/servicios/certificado-digital/certificado-digital-persona-fisica';
 const RU_PATH = '/ru/uslugi/cifrovoi-sertifikat-fizicheskogo-litsa';
